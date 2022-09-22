48c10d3ea501b3a609de5b67fc130a2377f9608a
yosh1 <97982992+11steve@users.noreply.github.com>
2022-09-22 19:33:10 +0300

    first commit

---
+++ LastNumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LastNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = 10;
+        System.out.println(a/b);
+    }
+
+}
